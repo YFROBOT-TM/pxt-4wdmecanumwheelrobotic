5d8cc20ad09498b566c19b6b466d38570241d6c8
finalvalue <finalvalue@foxmail.com>
2020-09-11 15:21:47 +0800

    V0.1.0

--- main.ts
+++ main.ts
@@ -1,24 +1,8 @@
-input.onButtonPressed(Button.B, function () {
+6input.onButtonPressed(Button.B, function () {
     Robotic.setMotor(Robotic.Motors.MAll, Robotic.Dir.CW, 206)
     basic.pause(1000)
     Robotic.setMotor(Robotic.Motors.MAll, Robotic.Dir.CCW, 156)
     basic.pause(1000)
     Robotic.stopMotor(Robotic.Motors.MAll)
 })
-music.playMelody("C5 B A G F E D C ", 500)
 Robotic.connectIrReceiver(DigitalPin.P1, Robotic.IrProtocol.NEC)
-basic.forever(function () {
-    if (Robotic.irButton() == Robotic.irButtonCode(Robotic.IrButton.Number_2)) {
-        Robotic.setMotor(Robotic.Motors.MAll, Robotic.Dir.CW, 200)
-    } else if (Robotic.irButton() == Robotic.irButtonCode(Robotic.IrButton.Number_4)) {
-        Robotic.setFourMotor(-200, 200, 200, -200)
-    } else if (Robotic.irButton() == Robotic.irButtonCode(Robotic.IrButton.Number_6)) {
-        Robotic.setFourMotor(200, -200, -200, 200)
-    } else if (Robotic.irButton() == Robotic.irButtonCode(Robotic.IrButton.Number_8)) {
-        Robotic.setMotor(Robotic.Motors.MAll, Robotic.Dir.CCW, 200)
-    } else if (Robotic.irButton() == Robotic.irButtonCode(Robotic.IrButton.Number_5)) {
-        Robotic.stopMotor(Robotic.Motors.MAll)
-    } else {
-    	
-    }
-})
